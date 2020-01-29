ef2945a4a455de76f5ff85cb769dd6f07caabc79
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-30 00:32:34 +0530

    trigger event  emit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,30 +1,26 @@
 pragma solidity ^0.5.0;
 
-contract TodoList{
-    uint public taskCount=0;
+contract TodoList {
+    uint256 public taskCount = 0;
 
-    struct Task{
-        uint id;
+    struct Task {
+        uint256 id;
         string content;
         bool completed;
     }
 
-    mapping(uint => Task) public tasks;
+    mapping(uint256 => Task) public tasks;
 
-    event TaskCreated(
-        uint id,
-        string content,
-        bool completed
-    )
+    event TaskCreated(uint256 id, string content, bool completed);
 
-    constructor() public{
+    constructor() public {
         createTask("Hello World");
     }
-    
-    function createTask(string memory _content) public{
-        taskCount++;
-        tasks[taskCount] = Task(taskCount, _content,false);
 
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 
 }
