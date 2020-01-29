bd313c030ecbe5d261efb5650dd03e5083d36c65
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-29 14:22:59 +0530

    App.todoList deployed

--- src/app.js
+++ src/app.js
@@ -39,6 +39,8 @@ App = {
     const todoList = await $.getJSON("TodoList.json");
     App.contracts.TodoList = TruffleContract(todoList);
     App.contracts.TodoList.setProvider(App.web3Provider);
+
+    App.todoList = await App.contracts.TodoList.deployed();
   }
 };
 $(() => {
